171419535e51842264e71aa926544f223863c6e8
shenja <evgenkhiznyak@gmail.com>
2024-03-02 16:33:37 +0200

    second

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/Header.css';
 import SearchBar from './SearchBar';
+import AtmosKajaLogo from '../assets/AtmosKajaLogoHome.png'
 
 const Header = () => {
   return (
     <div className="header">
       <Link to="/">
         <img
-          src="/AtmosKajaLogoHome.png"
+          src={AtmosKajaLogo}
           className="logo"
           alt="AtmosKaja Logo"
           style={{ width: '400px', height: 'auto' }}
